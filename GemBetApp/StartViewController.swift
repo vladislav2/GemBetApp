022c341c52e92655c866931e10154fb5f3b39a49
vladislav2 <etter.id75@gmail.com>
2021-03-18 16:15:23 +0200

    first comit

--- GemBetApp/StartViewController.swift
+++ GemBetApp/StartViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StartViewController.swift
 //  GemBetApp
 //
 //  Created by user on 16.03.2021.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
